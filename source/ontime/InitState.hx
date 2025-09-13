4e2405b9edfdd585ce8da9d43c9d709c7aa1fb4f
Sphis-Sinco <sinconsistencia@gmail.com>
2025-09-12 20:10:53 -0500

    goToPlayState define

--- source/ontime/InitState.hx
+++ source/ontime/InitState.hx
@@ -1,12 +1,20 @@
 package ontime;
 
+import flixel.FlxG;
 import flixel.FlxState;
+import haxe.macro.Compiler;
+import ontime.play.PlayState;
 
 class InitState extends FlxState
 {
 	override public function create()
 	{
 		super.create();
+
+		if (Compiler.getDefine('goToPlayState') == "1")
+		{
+			FlxG.switchState(() -> new PlayState());
+		}
 	}
 
 	override public function update(elapsed:Float)
