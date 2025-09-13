e0fcac2ecc33c6880a8c3eed25f1bb7e578db475
Sphis-Sinco <sinconsistencia@gmail.com>
2025-09-12 22:24:10 -0500

    macros are annoying

--- source/ontime/data/song/chart/SongChartNoteData.hx
+++ source/ontime/data/song/chart/SongChartNoteData.hx
@@ -7,17 +7,7 @@ class SongChartNoteData
 	public var step:Int;
 	public var beat:Int;
 
-	public var side(get, default):Int;
-
-	function get_side():Int
-	{
-		if (side < -1)
-			side = -1;
-		if (side > 1)
-			side = 1;
-
-		return side;
-	}
+	public var side:Int;
 
 	public function toString()
 	{
