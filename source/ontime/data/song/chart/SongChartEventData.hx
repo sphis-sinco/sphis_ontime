c6cfcdca7e684a72803f7ded0bd95d1b763fff24
Sphis-Sinco <sinconsistencia@gmail.com>
2025-09-12 21:24:01 -0500

    SongChartEventData

--- source/ontime/data/song/chart/SongChartEventData.hx
+++ source/ontime/data/song/chart/SongChartEventData.hx
@@ -1,3 +1,14 @@
 package ontime.data.song.chart;
 
-class SongChartEventData {}
+class SongChartEventData
+{
+	var step:Int;
+	var beat:Int;
+
+	var data:Dynamic;
+
+	public function toString()
+	{
+		return "SongChartEventData(step: " + this.step + ", beat: " + this.beat + ", data: " + this.data + ")";
+	}
+}
