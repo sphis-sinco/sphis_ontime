b7c2133ee45d8591720be5254ffa7db7cc442da2
Sphis-Sinco <sinconsistencia@gmail.com>
2025-09-12 22:30:48 -0500

    sides => notes and range from 1 - 3 now

--- source/ontime/data/song/chart/SongChartNoteData.hx
+++ source/ontime/data/song/chart/SongChartNoteData.hx
@@ -8,11 +8,11 @@ class SongChartNoteData
 	public var step:Int;
 	public var beat:Int;
 
-	public var side:Int;
+	public var note:Int;
 
 	public function toString()
 	{
-		return "SongChartNoteData(step: " + this.step + ", beat: " + this.beat + ", side: " + this.side + ")";
+		return "SongChartNoteData(step: " + this.step + ", beat: " + this.beat + ", note: " + this.note + ")";
 	}
 }
 #end
