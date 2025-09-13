5971f02260ca6b77424f84e56ba9d93a4c2269a8
Sphis-Sinco <sinconsistencia@gmail.com>
2025-09-12 22:22:06 -0500

    side should range from -1 to 1 only

--- source/ontime/data/song/chart/SongChartNoteData.hx
+++ source/ontime/data/song/chart/SongChartNoteData.hx
@@ -1,11 +1,23 @@
 package ontime.data.song.chart;
 
+import flixel.math.FlxMath;
+
 class SongChartNoteData
 {
 	public var step:Int;
 	public var beat:Int;
 
-	public var side:Int;
+	public var side(get, default):Int;
+
+	function get_side():Int
+	{
+		if (side < -1)
+			side = -1;
+		if (side > 1)
+			side = 1;
+
+		return side;
+	}
 
 	public function toString()
 	{
