5fa9cb36133c465fb0428370ef8f1930485ca450
Sphis-Sinco <sinconsistencia@gmail.com>
2025-09-12 19:56:21 -0500

    playstate

---
+++ source/ontime/play/PlayState.hx
@@ -0,0 +1,19 @@
+package ontime.play;
+
+import flixel.FlxState;
+import ontime.data.song.SongData;
+
+class PlayState extends FlxState
+{
+	public var beatTest:SongData;
+
+	override public function create()
+	{
+		super.create();
+	}
+
+	override public function update(elapsed:Float)
+	{
+		super.update(elapsed);
+	}
+}
